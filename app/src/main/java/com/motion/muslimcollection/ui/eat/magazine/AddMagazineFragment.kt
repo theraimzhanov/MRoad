7b7f466e39b805194bd0bf77fe208b462bca529f
atabe <atabekgithub23>
2022-04-09 16:17:42 +0600

    settings magazine

--- app/src/main/java/com/motion/muslimcollection/ui/eat/magazine/AddMagazineFragment.kt
+++ app/src/main/java/com/motion/muslimcollection/ui/eat/magazine/AddMagazineFragment.kt
@@ -1,22 +1,139 @@
 package com.motion.muslimcollection.ui.eat.magazine
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import com.motion.muslimcollection.R
 import com.motion.muslimcollection.core.base.BaseFragment
+import com.motion.muslimcollection.ext.showMessage
+import com.motion.muslimcollection.ext.visible
+import com.motion.muslimcollection.model.cafe_model.MagazineItem
+import com.motion.muslimcollection.network.result.Status
+import kotlinx.android.synthetic.main.fragment_add_magazine.*
+import kotlinx.android.synthetic.main.fragment_add_vacancy.*
+import kotlinx.android.synthetic.main.fragment_vacancy.*
+import org.koin.android.ext.android.inject
 
 
 class AddMagazineFragment : BaseFragment(R.layout.fragment_add_magazine) {
+    private val viewModel:MagazineViewModel by inject()
+
     override fun setupObservers() {
 
     }
 
     override fun showConnectedState() {
-
+addTextChangeListeners()
+        observeViewModel()
+        navigate()
     }
 
+    private fun navigate() {
+        btnAddMagazineItem.setOnClickListener {
+            addMagazineItem(etTextNameOfName.text.toString(),
+                etTextLocationOfMagazine.text.toString(),
+                etTextPhoneOfMagazine.text.toString(),checkData(rg_mg_certificate)
+                ,checkData(rg_mg_prayer_room),"")
+        }
+        addImageOfMagazine.setOnClickListener {
+
+        }
+        exitAddMagazine.setOnClickListener {
+            activity?.onBackPressed()
+        }
+    }
 
 
     override fun setupUI() {
         super.setupUI()
     }
+    private fun addTextChangeListeners() {
+        etTextPhoneOfMagazine.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
 
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                viewModel.resetErrorInputPhoneNumber()
+            }
 
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
+        etTextLocationOfMagazine.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                viewModel.resetErrorInputAddress()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
+        etTextNameOfName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                viewModel.resetErrorInputName()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+        })
+    }
+
+    private fun observeViewModel() {
+        viewModel.errorInputName.observe(viewLifecycleOwner) {
+            val massage = if (it){
+                getString(R.string.error)
+            }else{
+                null
+            }
+            til_nameOfMagazine.error = massage
+
+        }
+        viewModel.errorInputAddress.observe(viewLifecycleOwner) {
+            val massage = if (it){
+                getString(R.string.error)
+            }else{
+                null
+            }
+            til_locationOfMagazine.error = massage
+        }
+        viewModel.errorInputPhoneNumber.observe(viewLifecycleOwner) {
+            val massage = if (it){
+                getString(R.string.error)
+            }else{
+                null
+            }
+            til_phoneOfMagazine.error = massage
+        }
+        viewModel.shouldCloseScreen.observe(viewLifecycleOwner) {
+
+        }
+    }
+
+    private fun addMagazineItem( name:String?, address:String?
+                                 , phone:String,certificate:Boolean,msRoom:Boolean,img:String?){
+        if (img != null){
+            viewModel.postMagazineItem(name, phone, address, certificate, msRoom,MagazineItem.EMPTY_PAGE,"8:00")
+        } else{
+            viewModel.postMagazineItem(name, phone, address, certificate, msRoom, img,"8:00")
+        }
+    }
+    private fun checkData(radioGroup: RadioGroup):Boolean{
+       val button =activity?.findViewById<RadioButton>(radioGroup.checkedRadioButtonId)
+         var checked = false
+        button?.let {
+            checked = button.text.equals(resources.getString(R.string.hint_yes))
+        }
+        return checked
+    }
 }
--- app/src/main/java/com/motion/muslimcollection/ui/eat/magazine/MagazineViewModel.kt
+++ app/src/main/java/com/motion/muslimcollection/ui/eat/magazine/MagazineViewModel.kt
@@ -3,9 +3,13 @@ package com.motion.muslimcollection.ui.eat.magazine
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.motion.muslimcollection.core.base.BaseViewModel
+import com.motion.muslimcollection.ext.parseInputField
+import com.motion.muslimcollection.ext.parseNumber
 import com.motion.muslimcollection.model.cafe_model.MagazineItem
+import com.motion.muslimcollection.model.vacancy_model.VacancyItem
 import com.motion.muslimcollection.network.result.Resource
 import com.motion.muslimcollection.ui.eat.fastfood.FastFoodRepository
+import com.motion.muslimcollection.ui.vacancy.model.RezItem
 
 class MagazineViewModel(private val repository: MagazineRepository):BaseViewModel() {
 
@@ -29,12 +33,66 @@ class MagazineViewModel(private val repository: MagazineRepository):BaseViewMode
         get() = _item
 
     // post Magazine Item
-    fun postMagazineItem(magazineItem: MagazineItem) {
-        repository.postMagazineItem(magazineItem)
+    fun postMagazineItem(name:String?,phone: String?,address:String?,certificate:Boolean,msRoom:Boolean,img:String?,workTime:String) {
+        val nameMG = parseInputField(name)
+        val phoneMG = parseNumber(phone)
+        val addressNG = parseInputField(address)
+        val condition = validateInputName(nameMG,phoneMG,addressNG)
+        if (condition){
+            repository.postMagazineItem(MagazineItem(nameMG,phoneMG.toString()
+                ,addressNG, certificate = certificate, mosqueRoom = msRoom, images = img, worktime = workTime))
+            finishWork()
+        }
     }
 
-    //delete Magazine Item by id
-    fun deleteMagazineItem(id: Int){
-        repository.deleteMagazineItemById(id)
+    private val _errorInputName = MutableLiveData<Boolean>()
+    val errorInputName: LiveData<Boolean>
+        get() = _errorInputName
+
+    private val _errorInputPhoneNumber = MutableLiveData<Boolean>()
+    val errorInputPhoneNumber: LiveData<Boolean>
+        get() = _errorInputPhoneNumber
+
+    private val _errorInputAddress = MutableLiveData<Boolean>()
+    val errorInputAddress: LiveData<Boolean>
+        get() = _errorInputAddress
+
+    private val _shouldCloseScreen = MutableLiveData<Unit>()
+    val shouldCloseScreen: LiveData<Unit>
+        get() = _shouldCloseScreen
+
+    private fun validateInputName(name:String,phone:Long
+                                  ,address:String
+    ):Boolean{
+        var result = true
+        if (name.isBlank()) {
+            _errorInputName.value = true
+            result = false
+        }
+        if (phone <= 0) {
+            _errorInputPhoneNumber.value = true
+            result = false
+        }
+        if (address.isBlank()){
+            _errorInputAddress.value = true
+            result = false
+        }
+        return result
     }
+
+    fun resetErrorInputName() {
+        _errorInputName.value = false
+    }
+
+    fun resetErrorInputPhoneNumber() {
+        _errorInputPhoneNumber.value = false
+    }
+
+    fun resetErrorInputAddress() {
+        _errorInputAddress.value = false
+    }
+    private fun finishWork() {
+        _shouldCloseScreen.value = Unit
+    }
+
 }
